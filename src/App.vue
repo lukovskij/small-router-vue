81988de1361662612182634bc098d5854487080a
lukovskij <kravchuk1995@ukr.net>
2017-11-26 23:21:22 +0300

    create simple vue app with vue-router and api call

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+     <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <router-link to="/coins/bitcoin">bitcoin</router-link>
+    <router-link to="/coins/ethereum">ethereum</router-link>
+    <img src="./assets/logo.png">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import About from '@/components/About'
+import Hello from '@/components/Hello'
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+img{
+  display: block;
+  margin: 0 auto;
+  margin-top: 20px;
+}
+</style>
